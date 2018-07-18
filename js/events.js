c01b9e9ff04c29e4a11d2cb9d9e4f1bea0e2b3d5
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-18 22:47:35 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,14 @@
-//define functions here
+function getIt(){
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function() {
+    
+  })
+}
 
 $(document).ready(function(){
 
